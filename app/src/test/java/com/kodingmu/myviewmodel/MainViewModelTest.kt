4b9b121d44520fe1be621a1a7ba8f1d485dfaa76
Sulistiyanto <sulistiyanto404@gmail.com>
2019-08-04 07:22:47 +0700

    added unit testing

---
+++ app/src/test/java/com/kodingmu/myviewmodel/MainViewModelTest.kt
@@ -0,0 +1,64 @@
+package com.kodingmu.myviewmodel
+
+import org.junit.Assert
+import org.junit.Test
+import org.junit.Before
+import org.junit.Rule
+import org.junit.rules.ExpectedException
+
+class MainViewModelTest {
+
+    private var mainViewModel: MainViewModel? = null
+
+    @Before
+    fun init() {
+        mainViewModel = MainViewModel()
+    }
+
+    @Test
+    fun calculate() {
+        val width = "1"
+        val length = "2.4"
+        val height = "3"
+
+        mainViewModel?.calculate(width, height, length)
+        Assert.assertEquals(6, mainViewModel?.result)
+    }
+
+    @get:Rule
+    val thrown: ExpectedException = ExpectedException.none()
+
+    @Test
+    @Throws(NumberFormatException::class)
+    fun doubleInputCalculateTest() {
+        val width = "1"
+        val length = "2.4"
+        val height = "3"
+
+        thrown.expect(NumberFormatException::class.java)
+        thrown.expectMessage("For input string: \"2.4\"")
+        mainViewModel?.calculate(width, height, length)
+    }
+
+    @Test
+    @Throws(NumberFormatException::class)
+    fun characterInputCalculateTest() {
+        val width = "1"
+        val length = "A"
+        val height = "3"
+        thrown.expect(NumberFormatException::class.java)
+        thrown.expectMessage("For input string: \"A\"")
+        mainViewModel?.calculate(width, length, height)
+    }
+
+    @Test
+    @Throws(NumberFormatException::class)
+    fun emptyInputCalculateTest() {
+        val width = "1"
+        val length = ""
+        val height = "3"
+        thrown.expect(NumberFormatException::class.java)
+        thrown.expectMessage("For input string: \"\"")
+        mainViewModel?.calculate(width, height, length)
+    }
+}
